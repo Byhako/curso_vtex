4d43be330c4e603578cb4b4f13830fd19613f979
Ruben E Acosta <eldragondera1@gmail.com>
2021-04-12 15:49:48 -0500

    custom landing

---
+++ IO/theme/react/src/Icons.tsx
@@ -0,0 +1,202 @@
+import React from 'react';
+import {
+  MdWarning,
+  MdThumbUp,
+  MdStore,
+  MdRoom,
+  MdPrint,
+  MdPets,
+  MdPayment,
+  MdPanTool,
+  MdAccessibility,
+  MdFavorite,
+  MdFace,
+  MdDoneAll,
+  MdDescription,
+  MdDeleteForever,
+  MdDateRange,
+  MdCopyright,
+  MdClass,
+  MdCheckCircle,
+  MdBuild,
+  MdBackup,
+  MdAutorenew,
+  MdAssignmentInd,
+  MdAssessment,
+  MdAnnouncement,
+  MdAndroid,
+  MdAlarm,
+  MdAddShoppingCart,
+  MdAccountBox,
+  MdOpenWith,
+  MdAccountBalance,
+  MdAccessible,
+} from 'react-icons/md'
+import styles from './styles.css';
+
+const Icons = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Iconos del sitio</h1>
+      <div className={styles.container_icons}>
+        <div className={styles.item}>
+          <MdAccessibility size={'40px'} />
+          <span>MdAccessibility</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAccessible size={'40px'} />
+          <span>MdAccessible</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAccountBalance size={'40px'} />
+          <span>MdAccountBalance</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdOpenWith size={'40px'} />
+          <span>MdOpenWith</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAccountBox size={'40px'} />
+          <span>MdAccountBox</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAddShoppingCart size={'40px'} />
+          <span>MdAddShoppingCart</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAlarm size={'40px'} />
+          <span>MdAlarm</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAndroid size={'40px'} />
+          <span>MdAndroid</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAnnouncement size={'40px'} />
+          <span>MdAnnouncement</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAssessment size={'40px'} />
+          <span>MdAssessment</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAssignmentInd size={'40px'} />
+          <span>MdAssignmentInd</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdAutorenew size={'40px'} />
+          <span>MdAutorenew</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdBackup size={'40px'} />
+          <span>MdBackup</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdBuild size={'40px'} />
+          <span>MdBuild</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdCheckCircle size={'40px'} />
+          <span>MdCheckCircle</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdClass size={'40px'} />
+          <span>MdClass</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdCopyright size={'40px'} />
+          <span>MdCopyright</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdDateRange size={'40px'} />
+          <span>MdDateRange</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdDeleteForever size={'40px'} />
+          <span>MdDeleteForever</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdDescription size={'40px'} />
+          <span>MdDescription</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdDoneAll size={'40px'} />
+          <span>MdDoneAll</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdFace size={'40px'} />
+          <span>MdFace</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdFavorite size={'40px'} />
+          <span>MdFavorite</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdPanTool size={'40px'} />
+          <span>MdPanTool</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdPayment size={'40px'} />
+          <span>MdPayment</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdPets size={'40px'} />
+          <span>MdPets</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdPrint size={'40px'} />
+          <span>MdPrint</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdRoom size={'40px'} />
+          <span>MdRoom</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdStore size={'40px'} />
+          <span>MdStore</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdThumbUp size={'40px'} />
+          <span>MdThumbUp</span>
+        </div>
+        
+        <div className={styles.item}>
+          <MdWarning size={'40px'} />
+          <span>MdWarning</span>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Icons;
